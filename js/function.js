8ef948177583c2878efc770d666ac482c563b6ff
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-04-18 18:23:42 +0300

    mm

--- js/function.js
+++ js/function.js
@@ -2,27 +2,26 @@
  function itemClick(event) {
    let item = event.target;
    let name = item.dataset.name;
-   console.log(name);
    let price = Number(item.dataset.price);
    let ShoppingCart = document.querySelector("#ul_buy");
   
    // Check if item already in cart and remove if double clicked
    const existingItem = document.querySelector(`#ul_buy [data-name='${name}']`)
-   if (existingItem && event.detail === 2) {
-     total -= price;
-     existingItem.remove();
-   } else {
      let newLi = document.createElement('li');
      newLi.innerText = name;
      newLi.dataset.name = name;
      newLi.dataset.price = price;
+     newLi.addEventListener('dblclick',(event) => {
+    let itemCurt =  event.target;
+    itemCurt.remove()
+     })
      ShoppingCart.append(newLi);
      total += price;
    }
 
    const totalElement = document.querySelector("#total");
    totalElement.innerText = `${total}$`;
- }
+ 
 
  const list_items = [
    new classitems("banana", "12.5"),
@@ -38,7 +37,7 @@
     item.addEventListener("dblclick", itemClick);
  });
 
-document.querySelector("#app").addEventListener('dblclick', function(event) {
-  console.log(event.detail);
-});
+// document.querySelector("#app").addEventListener('dblclick', function(event) {
+//   console.log(event.detail);
+// });
 
