bc69e24b9111ae0758a55425cceef199d471b367
johny11111 <125285264+johny11111@users.noreply.github.com>
2023-04-18 16:54:05 +0300

    מ

--- js/function.js
+++ js/function.js
@@ -1,38 +1,43 @@
-let total = 0;
-function itemClick(event) {
-  let item = event.target;
-  let name = item.dataset.name;
-  let price = Number(item.dataset.price);
-  let ShoppingCart = document.querySelector("#ul_buy");
+ let total = 0;
+ function itemClick(event) {
+   let item = event.target;
+   let name = item.dataset.name;
+   let price = Number(item.dataset.price);
+   let ShoppingCart = document.querySelector("#ul_buy");
   
-  // Check if item already in cart and remove if double clicked
-  const existingItem = document.querySelector(`#ul_buy [data-name='${name}']`);
-  if (existingItem && event.detail === 2) {
-    total -= price;
-    existingItem.remove();
-  } else {
-    let newLi = document.createElement('li');
-    newLi.innerText = name;
-    newLi.dataset.name = name;
-    newLi.dataset.price = price;
-    ShoppingCart.append(newLi);
-    total += price;
-  }
+   // Check if item already in cart and remove if double clicked
+   const existingItem = document.querySelector(`#ul_buy [data-name='${name}']`);
+   if (existingItem && event.detail === 2) {
+     total -= price;
+     existingItem.remove();
+   } else {
+     let newLi = document.createElement('li');
+     newLi.innerText = name;
+     newLi.dataset.name = name;
+     newLi.dataset.price = price;
+     ShoppingCart.append(newLi);
+     total += price;
+   }
 
-  const totalElement = document.querySelector("#total");
-  totalElement.innerText = `${total}$`;
-}
+   const totalElement = document.querySelector("#total");
+   totalElement.innerText = `${total}$`;
+ }
 
-const list_items = [
-  new classitems("banana", "12.5"),
-  new classitems("kiwi", "10"),
-  new classitems("apple", "22"),
-  new classitems("orange", "15"),
-];
+ const list_items = [
+   new classitems("banana", "12.5"),
+   new classitems("kiwi", "10"),
+   new classitems("apple", "22"),
+   new classitems("orange", "15"),
+ ];
 
-list_items.forEach(item => item.render());
+ list_items.forEach(item => item.render());
 
-document.querySelectorAll("#ul_items li").forEach(item => {
-   item.addEventListener("click", itemClick);
-   item.addEventListener("dblclick", itemClick);
+ document.querySelectorAll("#ul_items li").forEach(item => {
+    item.addEventListener("click", itemClick);
+    item.addEventListener("dblclick", itemClick);
+ });
+
+document.querySelector("#app").addEventListener('dblclick', function(event) {
+  console.log(event.detail);
 });
+
